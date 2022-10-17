a17731d5228dba67856fbce8fe6e611e5549a6b2
Haruna Bah <harunabah101@gmail.com>
2022-10-17 11:14:19 -0600

    all pages uppdated

--- js/app.js
+++ js/app.js
@@ -71,15 +71,20 @@
     const modal__close = document.querySelector(".modal__close-btn");
     const modal__next = document.querySelector(".modal__next-btn");
     const modal__prev = document.querySelector(".modal__prev-btn");
+    const modal__title = document.querySelector(".modal__title");
 
     project__images.forEach((image) => {
       const image__list = image.getAttribute("data-project-images");
+      const image__title = image.getAttribute("data-project-title");
+
+
       image.addEventListener("click", (event) => {
         event.preventDefault();
-
         if (image__list) {
           const new__image__list = JSON.parse(image__list);
           const first__image = new__image__list[0];
+          document.querySelector('body').classList.add('no-scroll');
+          modal__title.innerText = image__title || "Portfolio Project";
           modal__image.src = first__image;
           modal__image.setAttribute('data-current-image', first__image);
           modal.style.display = "block";
@@ -90,6 +95,7 @@
     // end forEach
 
     modal__close.addEventListener("click", () => {
+      modal__image.src = "";
       modal.style.display = "none";
     });
 
